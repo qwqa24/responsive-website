ad60d8e4ab2c12f7c12b2cfae8059b6508fe09ca
qwqa24 <84373987+qwqa24@users.noreply.github.com>
2021-11-03 17:46:58 +0900

    html

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//넷바 색상 변경 
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height
+document.addEventListener("scroll", () => {
+if(window.scrollY > navbarHeight) {
+    navbar.classList.add('nacvar--dark');
+} else {
+    navbar.classList.remove('navbar--dark');
+}
+});
+
+//넷바메뉴 선택시 해당 부분으로 이동하기
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event)=> {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link==null) {
+        return;
+    }
+    scrollIntoView(link);
+    });
+//contactMeBtn 버튼 이동하기
+const cintactMeBtn = document.querySelector(".home__contact");
+contactMeBtn.addEventListener("click", ()=> {
+    scrollIntoView("contact");
+});
+    function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollToView({behavior : "smooth" })
+    }
